5d0bc60eaa0da2dad1798f4d0fd9a3c0e1e16aa0
sourabh-yalagod <sourabhofficial99804@gmail.com>
2024-08-02 22:15:45 +0530

    update vite file

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '/api': {
         target: 'https://videotube-auro.onrender.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
     },
   },
