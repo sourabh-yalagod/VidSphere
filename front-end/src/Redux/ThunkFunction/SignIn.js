93b94f346e0ea50e05e4707bf8a868db8b09880c
sourabh-yalagod <sourabhofficial99804@gmail.com>
2024-08-02 21:59:13 +0530

    prefixed BaseURl

--- front-end/src/Redux/ThunkFunction/SignIn.js
+++ front-end/src/Redux/ThunkFunction/SignIn.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 const signIn = createAsyncThunk("signIn", async (formData) => {
   try {
-    const response = await axios.post(`${process.env.BASE_URL}/api/v1/users/login`, formData, {
+    const response = await axios.post(`api/v1/users/login`, formData, {
       withCredentials: true,
     });
     return response.data;
