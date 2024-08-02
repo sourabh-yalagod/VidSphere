00a9e60456467e69f6a2cf3bc6d13766fdf08a98
sourabh-yalagod <sourabhofficial99804@gmail.com>
2024-08-02 22:07:00 +0530

    updated vite.js file

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
   server: {
     proxy: {
-      "/api": {
-        target: "https://videotube-auro.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+      '/api': {
+        target: 'https://videotube-auro.onrender.com',
+        // changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
